bcbcfc85963b7e563c16a316a3db6432af7c4a7b
hobao-133 <kevinbh133@gmail.com>
2024-11-19 21:47:33 +0700

    first commit

---
+++ README.md/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace SystemTime{
+    class Program{
+        static void Main(string[]args){
+            Console.WriteLine("Hey There!");
+            DateTime localDate = DateTime.Now;
+            System.Console.WriteLine("Datetime Now is :" + localDate);
+        }
+            }
+}
